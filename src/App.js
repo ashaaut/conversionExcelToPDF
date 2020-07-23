aff936c52a69c810183413417ce0629181d7cdd3
ashaaut <ashaautade1998@gmail.com>
2020-07-23 12:23:31 +0530

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+import { hot } from 'react-hot-loader/root';
+import Login from './component/login'
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>
+          hii
+        </h1>
+      </>
+    );
+  }
+}
+
+export default hot(App);
